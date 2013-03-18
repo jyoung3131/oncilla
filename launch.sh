58da352fa04cc48f94e04b0569acc1e732ae7f05
Alexander Merritt <merritt.alex@gatech.edu>
2013-03-18 14:17:46 -0400

    Changed permissions, removed obsolete files

--- launch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mpirun -np 2 -npernode 1 -hostfile bin/nodefile bin/oncillamem
--- test/gdb.cmd
+++
@@ -1,4 +0,0 @@
-set environment LD_LIBRARY_PATH ../lib:/extoll2/lib:/extoll2/mpi/openmpi-1.6.1/lib:/usr/local/cuda-5.0/lib64:/usr/local/cuda-5.0/lib:/usr/local/lib
-set args 2 20
-b 15
-run
