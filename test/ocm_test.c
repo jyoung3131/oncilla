aab8f18f52cc558a46363dba75b7bdd622e54d22
Se Hoon Shon <sehoon@shiva.cc.gt.atl.ga.us>
2013-04-18 10:45:03 -0400

    bw test

--- test/ocm_test.c
+++ test/ocm_test.c
@@ -200,6 +200,88 @@ fail:
   return -1;
 }
 
+static int read_write_bw_test(){
+  ocm_alloc_t a;
+  void *buf;
+  size_t buf_len, remote_len;
+  ocm_alloc_param_t alloc_params;
+  ocm_param_t copy_params;
+
+  if (0 > ocm_init()) {
+    printf("Cannot connect to OCM\n");
+    return -1;
+  }
+
+  //Create a structure that is used to configure the allocation on each endpoint
+  alloc_params = calloc(1, sizeof(struct ocm_alloc_params));
+  alloc_params->local_alloc_bytes = local_size_B;
+  alloc_params->rem_alloc_bytes = rem_size_B;
+  #ifdef INFINIBAND
+  alloc_params->kind = OCM_REMOTE_RDMA;
+  #endif
+  #ifdef EXTOLL
+  alloc_params->kind = OCM_REMOTE_RMA;
+  #endif
+
+  a = ocm_alloc(alloc_params);
+  if (!a) {
+    printf("ocm_alloc failed on remote size %lu\n", rem_size_B);
+    return -1;
+  }
+
+  copy_params = (ocm_param_t)calloc(1,sizeof(struct ocm_params));
+  copy_params->src_offset = 0;
+  copy_params->dest_offset = 0;
+  copy_params->bytes = local_size_B;
+  copy_params->op_flag = 0;
+
+  //Use a one-sided copy since we are copying from a local-remote IB
+  //paired object
+  if(ocm_copy_onesided(a, copy_params)){
+    printf("ocm_copy_onesided (read) failed\n");
+    goto fail;
+  } 
+
+  copy_params->op_flag = 1;
+
+  if(ocm_copy_onesided(a, copy_params)){
+    printf("ocm_copy_onesided (write) failed\n");
+    goto fail;
+  } 
+
+  if (ocm_localbuf(a, &buf, &buf_len)) {
+    printf("ocm_localbuf failed\n");
+    goto fail;
+  }
+  printf("local buffer size %lu @ %p\n", buf_len, buf);
+
+  if (ocm_is_remote(a)) {
+    if (!ocm_remote_sz(a, &remote_len)) {
+      printf("alloc is remote; size = %lu\n", remote_len);
+    } else {
+      printf("alloc is local\n");
+    }
+  }
+
+  free(alloc_params);
+  free(copy_params);
+
+  if (0 > ocm_tini()) {
+    printf("ocm_tini failed\n");
+    return -1;
+  }
+
+  printf("OCM test completed successfully\n");
+  return 0;
+
+fail:
+
+  free(alloc_params);
+  free(copy_params);
+  if (0 > ocm_tini())
+    printf("ocm_tini failed\n");
+  return -1;
+}
 static int copy_twosided_test(uint64_t local_size_B,uint64_t rem_size_B){
   ocm_alloc_t local_alloc, remote_alloc, local_alloc2;
   #ifdef CUDA
@@ -319,7 +401,7 @@ int main(int argc, char *argv[])
 usage:
     fprintf(stderr, "Usage: %s <which test> <test_suboption> <allocation size 1 in MB (alloc1)>"
         " <allocation size 2 in MB (alloc2)>\n "
-	"\twhich test: 1=allocation; 2=copy-onesided; 3=copy-twosided\n" 
+	"\twhich test: 1=allocation; 2=copy-onesided; 3=copy-twosided; 4= read/write BW test\n" 
 	"\tSuboptions: 1=allocate host memory; 2=allocate GPU memory; 3=allocate IB buffer (alloc1-local, alloc2-remote)\n"
 	" \t\t4=allocate EXTOLL buffer (alloc1-local, alloc2-remote)\n", argv[0]);
     return -1;
