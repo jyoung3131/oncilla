fb283f4470004156e38f28107ac3b0ddc39d4a66
Jeff Young <jyoung9@gatech.edu>
2013-05-16 15:15:45 -0400

    Added gitattributes and a simple git merge driver to preserve timing tags.

---
+++ keepMine.sh
@@ -0,0 +1,5 @@
+# We want to keep our version of the file when there is a conflict to make sure timing flags aren't removed
+# Nothing to do: %A (the second parameter) already contains our version
+# Just indicate the merge has been successfully "resolved" with the exit status
+#***Note that this script must be somewhere in your $PATH
+exit 0
